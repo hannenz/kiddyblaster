8816a009c9953ab94e69252df30db902a0fc564c
Johannes Braun <j.braun@agentur-halma.de>
2019-12-28 09:35:06 +0100

    Added lcd_refresh function

--- src/i2c_lcd.c
+++ src/i2c_lcd.c
@@ -71,6 +71,22 @@ void lcd_loc(int line) {
 
 
 /**
+ * Refresh the content of the LCD
+ *
+ * Needed as workaround to be called periodically to avoid gibberish content on
+ * the LCD
+ *
+ * @param void
+ * @return void
+ */
+void lcd_refresh() {
+	lcd_puts(LCD_LINE_1, content[0]);
+	lcd_puts(LCD_LINE_2, content[1]);
+}
+
+
+
+/**
  * Output string on LCD display
  *
  * @param int           Line (0x80 or 0xc0), use constants LCD_LINE_1 ...
